bc515078bc75fddf7a55d3edb5c2044e1173ba3f
Azhar <darkcoders0101@gmail.com>
2023-12-12 00:06:39 +0530

    Adding code Dark and Light Mode

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,23 @@
-/**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
- */
 
 "use strict";
+
+const /** {NodeElement} */ $HTML = document.documentElement;
+let /** {Boolean} */ isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if(sessionStorage.getItem("theme")){
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+}else {
+    $HTML.dataset.theme = isDark ? "dark" : "light";
+}
+
+const changeTheme = function () {
+    console.log("theme changed");
+    isDark = sessionStorage.getItem("theme");
+    sessionStorage.setItem("theme", isDark === "light" ? "dark" : "light");
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+}
+
+window.addEventListener("load", () => {
+    const /** {NodeElement} */ $themeBtn = document.querySelector("[data-theme-toggler]");
+    $themeBtn.addEventListener("click", changeTheme);
+});
