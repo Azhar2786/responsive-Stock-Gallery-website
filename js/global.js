197d5546ceb0cddf4a9dc7f94e692beb9d4d0d3f
Azhar <darkcoders0101@gmail.com>
2023-12-12 00:05:45 +0530

    adding scroll effect on header and ripple effect

--- js/global.js
+++ js/global.js
@@ -1,6 +1,27 @@
-/**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
- */
 
 "use strict";
+
+
+/**
+ * import
+ */
+
+import { ripple } from "./utils/ripple.js";
+
+/**
+ * Header on-scroll
+ */
+
+const /** {NodeElement} */ $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+/**
+ * Add ripple effect
+ */
+
+const /** {NodeList}  */ rippleElements = document.querySelectorAll("[data-ripple]");
+
+rippleElements.forEach(rippleElement => ripple(rippleElement));
