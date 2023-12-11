bc550f4a09f363d3a1d006af7dac3be5a7f809c8
Azhar <darkcoders0101@gmail.com>
2023-12-12 00:06:17 +0530

    adding code for ripple effect

--- js/utils/ripple.js
+++ js/utils/ripple.js
@@ -1,6 +1,46 @@
-/**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
- */
 
 "use strict";
+
+
+
+/**
+ * @param {Node} rippleElement Element for ripple effect
+ */
+
+export const ripple = function(rippleElement){
+    rippleElement.addEventListener("pointerdown", function (e) {
+        e.stopImmediatePropagation();
+
+        const /** {NodeElement} */ ripple = document.createElement("div");
+        ripple.classList.add("ripple");
+
+        this.appendChild(ripple);
+
+        const removeRipple = () => {
+            ripple.animate({
+                opacity: 0
+            },{ fill: "forwards", duration: 200});
+
+            setTimeout(() =>{
+                ripple.remove();
+            }, 1000);
+        }
+
+        this.addEventListener("pointerup", removeRipple);
+        this.addEventListener("pointerleave", removeRipple);
+
+        const isNotIconButton = !this.classList.contains("icon-btn");
+
+        if(isNotIconButton) {
+            /**
+             * {Number}
+             */
+            rippleSize = Math.max(this.clientWidth, this.clientHeight);
+
+            ripple.style.top = `${e.layerY}px`;
+            ripple.style.left = `${e.layerX}px`;
+            ripple.style.width = `${rippleSize}px`;
+            ripple.style.height = `${rippleSize}px`;
+        }
+    });
+}
